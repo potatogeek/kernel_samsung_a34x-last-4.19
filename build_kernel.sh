559148f3d8f0dd06e21470c119ddf504e2354870
potatogeek <gnogueira3164@gmail.com>
2025-05-27 14:47:12 -0300

    setup ksu

--- build_kernel.sh
+++ build_kernel.sh
@@ -12,7 +12,7 @@ export TARGET_BUILD_VARIANT=user
 export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 a34x_defconfig
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 a34x_defconfig a34.config
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y LLVM=1 LLVM_IAS=1 -j16
 
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
